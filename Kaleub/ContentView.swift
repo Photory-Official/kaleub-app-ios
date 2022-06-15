0f0f5975db16c4e3608d08c9d1b7b3fb137d2fde
lgvv <lgw9898@naver.com>
2022-06-16 02:18:48 +0900

    [추가] 프로젝트 파일을 생성합니다.

---
+++ Kaleub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kaleub
+//
+//  Created by Hamlit Jason on 2022/06/15.
+//
+
+import SwiftUI
+import KaleubSDK
+
+struct ContentView: View {
+//    let t = APIClient().urlString
+    var body: some View {
+        Text("text")
+            .padding()
+        Text("Text")
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Kaleub/KaleubApp.swift
@@ -0,0 +1,19 @@
+//
+//  KaleubApp.swift
+//  Kaleub
+//
+//  Created by Hamlit Jason on 2022/06/15.
+//
+
+import SwiftUI
+import KaleubSDK
+
+@main
+struct KaleubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
