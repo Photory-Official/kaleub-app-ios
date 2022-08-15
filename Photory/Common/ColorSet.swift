06f8287c75a9604b16b8894206c0e61e598fa6a0
lgvv <lgw9898@naver.com>
2022-08-15 12:51:32 +0900

    [추가] gitignore 및 common

--- Photory/Common/ColorSet.swift
+++ Photory/Common/ColorSet.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct ColorSet {
     /// 앱 내에서 사용되는 프라이머리 컬러
     static let primary = Color(#colorLiteral(red: 0.9411765337, green: 0.9411765933, blue: 0.9411765337, alpha: 1))
-    static let background = Color(#colorLiteral(red: 0.9411765337, green: 0.9411765933, blue: 0.9411765337, alpha: 1))
+    static let background = Color.black
     static let shadowColor = Color.gray
-    
 }
